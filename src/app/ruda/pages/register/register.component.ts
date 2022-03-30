1cb5b9a50369d3758ec03427fc3bc5ef761dee4e
alejandroalvarezdev <alejandroalvarezdev@gmail.com>
2022-03-29 22:51:25 -0600

    Changes Desicion Ok

--- src/app/ruda/pages/register/register.component.ts
+++ src/app/ruda/pages/register/register.component.ts
@@ -31,7 +31,8 @@ export class RegisterComponent implements OnInit {
     otrosDep:[],
     clasificacion:[],
     pruebaPrincipal:[],
-    rfc:['']
+    rfc:[''],
+    gastosMed:['']
     
   })
   
